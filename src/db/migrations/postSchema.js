5811cf3d9ac3e3c3ab4f8a17778fe068ba97ff38
Théo Battut <theo.batt.ju@gmail.com>
2020-09-29 19:02:13 +0200

    push jour 1

---
+++ src/db/migrations/postSchema.js
@@ -0,0 +1,11 @@
+const { Schema } = require('mongoose');
+
+const PostSchema = new Schema({
+    title: String,
+    description: String,
+    image: String,
+    body: String,
+    published: Boolean,
+}, { timestamps: true });
+
+module.exports = PostSchema;
